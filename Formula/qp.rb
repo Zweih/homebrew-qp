782f38e5c86b3b72755350b58faefb9e4e57ef93
Fernando N <me@fernandonunez.io>
2025-05-21 04:17:18 +0000

    Add linuxbrew.

--- Formula/qp.rb
+++ Formula/qp.rb
@@ -3,22 +3,37 @@ class Qp < Formula
   homepage "https://github.com/Zweih/qp"
   version "5.47.1"
   license "GPL-3.0-only"
+
   sha256_amd64 = "60248429cd57c6cde47b94318418b9861a198da8c2652515fba156a03cebed44"
   sha256_arm64 = "572061992cdc58a3e37c8e4f6ab0fff8fe0f69bd39caa2e6cdb9b741b25f8a03"
 
+  sha256_linux_x86_64 = "FILLER"
+  sha256_linux_aarch64 = "FILLER"
+
   on_macos do
     if Hardware::CPU.intel?
-      url "#{homepage}/releases/download/v#{version}/qp-v#{version}-darwin_amd64.tar.gz"
+      url "#{homepage}/releases/download/v#{version}/qp-v#{version}-brew-darwin_amd64.tar.gz"
       sha256 sha256_amd64
     elsif Hardware::CPU.arm?
-      url "#{homepage}/releases/download/v#{version}/qp-v#{version}-darwin_arm64.tar.gz"
+      url "#{homepage}/releases/download/v#{version}/qp-v#{version}-brew-darwin_arm64.tar.gz"
       sha256 sha256_arm64
+    end
+  end
+
+   on_linux do
+    if Hardware::CPU.intel?
+      url "#{homepage}/releases/download/v#{version}/qp-v#{version}-brew-x86_64.tar.gz"
+      sha256 sha256_linux_amd64
+    elsif Hardware::CPU.arm?
+      url "#{homepage}/releases/download/v#{version}/qp-v#{version}-brew-aarch64.tar.gz"
+      sha256 sha256_linux_arm64
     end
   end
 
   def install
     bin.install "qp"
     man1.install "qp.1"
+    doc.install "NEWS"
   end
 
   test do
