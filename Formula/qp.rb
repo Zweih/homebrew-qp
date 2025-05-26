61fd12b9e44c084c4ab5487d935fe1522866240f
Fernando Nunez <15152269+Zweih@users.noreply.github.com>
2025-05-26 15:22:03 -0600

    Update qp formula to v5.60.0 (#7)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/qp.rb
+++ Formula/qp.rb
@@ -1,14 +1,14 @@
 class Qp < Formula
   desc "qp - Query Packages. A CLI utility for querying installed packages."
   homepage "https://github.com/Zweih/qp"
-  version "5.58.0"
+  version "5.60.0"
   license "GPL-3.0-only"
 
-  sha256_amd64 = "c0ac2b5673f8b71dbb799547f80dcc71c3feb3dad1d5f7865712a57aa5f6cdd0"
-  sha256_arm64 = "559707d50f6e112e4d384e55a52c85df26afb01caf016c8e087c68f693019300"
+  sha256_amd64 = "1c804230a257d08f715f165f4b02561323313e1eb8086860e79033f7f06027ce"
+  sha256_arm64 = "b3ead9ba2071c74061235c1f9cb106b35160d37a68b256328293438deda963e1"
 
-  sha256_linux_x86_64 = "70880d850eeaa8573e2f0112929c76c630984f6977ff6d7fcdcd9b9b420aa385"
-  sha256_linux_aarch64 = "527fc3285458e4195594a9a4ffd71139afa15b45426509a1059e78219c94dfbf"
+  sha256_linux_x86_64 = "f7df8d44b3bd63b9809fbbdb10b1a183230812f55470a4d8853792e99ba49c99"
+  sha256_linux_aarch64 = "dc40ec11a15e4f054cf28f90324cfda52d2da2cc8fe000613ad06c631264bf2c"
 
   on_macos do
     if Hardware::CPU.intel?
